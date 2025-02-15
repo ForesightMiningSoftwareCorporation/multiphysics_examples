421bd380b935eae9d0fddeaa7eb86b2f9a592afb
Sébastien Crozet <sebcrozet@dimforge.com>
2025-02-15 18:43:19 +0100

    feat: introduce the bevy_wgsparkl crate, the begging of a bevy plugin for wgsparkl

---
+++ crates/bevy_wgsparkl/src/lib.rs
@@ -0,0 +1,8 @@
+pub mod step;
+pub mod instancing3d;
+pub mod prep_vertex_buffer;
+pub mod startup;
+pub mod components;
+pub mod resources;
+
+// TODO: define a plugin by extractin the relevant pieces from sandbox/src/main.rs
